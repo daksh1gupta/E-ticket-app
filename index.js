5ef4b675ff309f2bbf09b2f8dde9b92d1f46d2b5
daksh gupta <168224098+daksh1gupta@users.noreply.github.com>
2026-02-11 18:14:24 +0530

    Add flight.ejs file & update index.js file

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const path = require("path");
+const { title } = require("process");
 
 const port = 8080;
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"ui"));
-
+//home route
 app.get("/",(req, res)=>{
     res.render("index",{
         title: "Home_page"
     });
+});
+
+//flight route
+app.get("/flight",(req,res)=>{
+     res.render("flight",{
+        title: "Flight"
+     });
 });
 
 app.listen(port,()=>{
